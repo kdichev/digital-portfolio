4a0f70ac7aa37c3b270018921e6479d1b462e514
kdichev <konstantin.dichev@gmail.com>
2017-07-13 16:12:50 +0200

    add landing page structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+
+const TITLE = "SasheF and Koko"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <MuiThemeProvider>
+        <div>
+          <AppBar
+            title={TITLE}
+            showMenuIconButton={false}
+          />
+          <div>
+            <h1>We create digital products we are proud of. Tell us your idea and we’ll do everything else - from research and planning to custom design and development. We help startups and businesses achieve their goals and score big.</h1>
+            <a href="">see projects</a>
+          </div>
+          <div>
+            <h1>Team overview</h1>
+            <h4>Konstantin Dichev</h4>
+            <p>CEO, co-founter | Front End development</p>
+            <h4>Aleksandar Aleksandrov</h4>
+            <p>CTO, co-founter | Back End development</p>
+          </div>
+          <div>
+            <h1>Companies we worked with</h1>
+          </div>
+          <div>
+            <h1>Projects</h1>
+          </div>
+          <div>
+            <h1>Have a project for us?</h1>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
