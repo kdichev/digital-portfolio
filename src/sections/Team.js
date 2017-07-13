7d28c1635099c574b4ca11e644ba63466eea26e1
Konstantin-home <konstantin.dichev@gmail.com>
2017-07-13 21:37:08 +0200

    add team pictures

--- src/sections/Team.js
+++ src/sections/Team.js
@@ -8,10 +8,16 @@ class Team extends Component {
         <Title text="Team overview"/>
         <div className="col-md-3"></div>
         <div className="col-md-4">
+          <div style={{borderRadius: "50%", width: 200, height: 200}}>
+            <img src="http://web-lom.com/img/Konstantin_Dichev.png" />
+          </div>
           <h4>Konstantin Dichev</h4>
           <p>CEO, co-founter | Front End development</p>
         </div>
         <div className="col-md-4">
+          <div style={{borderRadius: "50%", width: 200, height: 200}}>
+            <img src="http://web-lom.com/img/Aleksandar_Aleksandrov_sashef.png" />
+          </div>
           <h4>Aleksandar Aleksandrov</h4>
           <p>CTO, co-founter | Back End development</p>
         </div>
