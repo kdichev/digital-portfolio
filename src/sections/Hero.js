2256cb2468a27c70ab6ba97aa1002e9097c1119d
Konstantin-home <konstantin.dichev@gmail.com>
2017-07-14 00:23:34 +0200

    changes in hero

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import VideoCover from './../components/VideoCover'
-
+import RaisedButton from 'material-ui/RaisedButton'
 class Hero extends Component {
   render() {
     return (
-          <div className="row">
-            <VideoCover />
-            <div
-            >
-            <h1>We create digital products we are proud of. Tell us your idea
-               and we’ll do everything else -
-              from research and planning to custom design and development.
-              We help startups and businesses achieve their goals and score big
-              .</h1>
+          <div className="row" style={{height: 700, backgroundColor: "#578CA9"}}>
+            {/* <VideoCover /> */}
+            <div style={{textAlign: "center", padding: 300}}>
+            <h1 style={{color: "white"}}>We create digital products we are proud of. Tell us your idea.</h1>
+            <p style={{color: "white", fontSize: 17}}>We have loads of experience and loads of power!!!</p>
+              <RaisedButton label="Learn more" backgroundColor="#10a887" labelColor="white" labelStyle={{paddingLeft: 60, paddingRight: 60}} style={{height: 50}}/>
             </div>
           </div>
     );
