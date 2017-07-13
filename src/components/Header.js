b43430f8aa35a6aa611469341be6fa8fdbdb1048
Konstantin-home <konstantin.dichev@gmail.com>
2017-07-14 00:24:27 +0200

    add header functionality onScroll

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,46 @@ import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 const TITLE = "Sashko"
 class Header extends Component {
+  constructor(props) {
+  super(props);
+    this.state = {
+      style: {backgroundColor: "#578ca9", position: "none"},
+      titleColor: {color: "white"},
+      zDepth: 0
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = () => {
+    if (document.body.scrollTop > 64) {
+      this.setState({
+        style: {backgroundColor: "white", position: "fixed"},
+        titleColor: {color: "black"},
+        zDepth: 1
+      })
+    } else {
+      this.setState({
+        style: {backgroundColor: "#578ca9", position: "initial"},
+        titleColor: {color: "white"},
+        zDepth: 0
+      })
+    }
+  }
+
+  componentWillUnmount = () => {
+    window.removeEventListener('scroll', this.handleScroll());
+  }
   render() {
     return (
       <AppBar
         title={TITLE}
         showMenuIconButton={false}
-        style={{backgroundColor: "white"}}
-        titleStyle={{color: "black"}}
+        style={this.state.style}
+        titleStyle={this.state.titleColor}
+        zDepth={this.state.zDepth}
       />
     );
   }
